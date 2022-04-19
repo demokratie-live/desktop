22a1c28c92548fefd0cd26a81ba301e82add2931
Manuel Ruck <git@manuelruck.de>
2022-04-19 20:34:23 +0200

    log webpack config

--- next.config.js
+++ next.config.js
@@ -3,40 +3,37 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer(() => ({
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
-  compiler: {
-    styledComponents: true,
-  },
-  webpack: (config2) => {
-    return {
-      resolve: {
-        ...(config2.resolve || {}),
-        alias: {
-          ...(config2.resolve.alias || {}),
-          'react-native': require.resolve('react-native-web'),
-          'react-native-svg': require.resolve('react-native-svg-web'),
-          'styled-components/native': require.resolve('styled-components'),
-        },
-        extensions: [
-          ...(config2.resolve.extensions || []),
-          '.web.js',
-          '.web.ts',
-          '.web.tsx',
-        ],
-      },
-    };
-  },
-}));
+module.exports = () =>
+  withBundleAnalyzer({
+    eslint: {
+      dirs: ['.'],
+    },
+    poweredByHeader: false,
+    trailingSlash: true,
+    basePath: '',
+    // The starter code load resources from `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    reactStrictMode: true,
+    images: {
+      formats: ['image/avif', 'image/webp'],
+    },
+    webpack: (config) => {
+      console.log(config);
+      // eslint-disable-next-line no-param-reassign
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        'react-native$': 'react-native-web',
+        'react-native-svg$': 'react-native-svg-web',
+        'styled-components/native$': 'styled-components',
+      };
+      // eslint-disable-next-line no-param-reassign
+      config.resolve.extensions = [
+        '.web.js',
+        '.web.ts',
+        '.web.tsx',
+        ...config.resolve.extensions,
+      ];
+      return config;
+    },
+  });
